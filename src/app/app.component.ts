85fbe4035c04a613833c6d826730ebb8b1ce0de4
Gorka29 <gorkaruizaraujo@gmail.com>
2024-11-24 01:25:28 +0100

    mejoras varias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardListComponent, CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
